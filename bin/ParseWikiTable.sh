c27b66a3eebcda13013219fe2132caacf551d273
sheacd7 <shea.colin.d@gmail.com>
2015-08-13 13:29:08 -0400

    parse table into station array
    station array: "state, city, station, frequency"

--- bin/ParseWikiTable.sh
+++ bin/ParseWikiTable.sh
@@ -11,4 +11,27 @@ IN_FILE="/cygdrive/c/users/sheacd/temp/PublicRadio/NPR-Stations-By-State-Edit.tx
 # remove all html tags and blank lines
 mapfile -t arr < <(sed "s/<[^>]\{1,\}>//g" "${IN_FILE}" | sed '/^$/d')
 
+# get idcs for state rows
+# state rows have suffix '[edit]'
+# states array format: [#]=[row_state]
+declare -a states
+for (( idx=0 ; idx<${#arr[@]} ; idx++ )); do 
+  [[ "${arr[$idx]}" =~ edit\]$ ]] && states+=("$idx"_"${arr[$idx]%%\[edit\]}")
+done
+
+# build list of "state, city, station, frequency" from arr, state idcs
+declare -a stations
+for (( idx=0 ; idx<$(( ${#states[@]} - 1 )) ; idx++ )); do
+  b_row=${states[$idx]%%_*}
+  state=${states[$idx]##*_}
+  e_row=${states[$(($idx + 1))]%%_*}
+  length=$(( ($e_row - $b_row - 1)/3 ))
+  for (( row=1 ; row<$length ; row+=3 )); do 
+    stations+=($state ${arr[@]:$(( $b_row + $row )):3})
+  done
+
+done
+
+printf '%s\n' "${stations[@]}"
+
 
